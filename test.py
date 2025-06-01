85f11e77515b3a347fa3d3ba1c734f1b63feccf0
d-hye-n <qordkadkwla@gmail.com>
2025-06-02 02:13:45 +0900

    initial commit

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-import numpy as np
+import torch
 
-# 임의의 값으로 채워진 배열 생성
-random_mat = np.random.random((2,2)) # 임의의 값으로 채워진 배열 생성
-print(random_mat)
+t = torch.tensor([0., 1., 2., 3., 4., 5., 6.])
+# 2 x 1 Vector + 1 x 2 Vector
+m1 = torch.FloatTensor([[1, 2]])
+m2 = torch.FloatTensor([[3], [4]])
+print(m1 + m2)
