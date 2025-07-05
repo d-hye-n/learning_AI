8c1a62b3a205dc2e80231063d6d0edb8051d22f0
d-hye-n <qordkadkwla@gmail.com>
2025-07-06 03:44:09 +0900

    Loss Function

--- tensorflow_study.py
+++ tensorflow_study.py
@@ -1,4 +1,16 @@
 import tensorflow as tf
 
-tensor = tf.constant(3)
-print(tensor)
+키 = [170, 180, 175, 160]
+신발 = [260, 270, 265, 255]
+
+a = tf.Variable(0.1)
+b = tf.Variable(0.2)
+
+def 손실함수():
+    return tf.square(신발 - (키 * a + b))
+
+opt = tf.keras.optimizers.Adam(learning_rate=0.1)
+
+for i in range(1000):
+    opt.minimize(손실함수, var_list=[a,b])
+    print(a.numpy(),b.numpy())
