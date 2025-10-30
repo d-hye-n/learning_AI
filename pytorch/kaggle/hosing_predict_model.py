771592e0b955b7dbcf8218f6715b0312c1c6733f
d-hye-n <qordkadkwla@gmail.com>
2025-10-31 01:23:54 +0900

    Kaggle Housing competition

--- pytorch/kaggle/hosing_predict_model.py
+++ pytorch/kaggle/hosing_predict_model.py
@@ -10,9 +10,21 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import cross_val_score
 pd.set_option('display.max_columns', None)
 
-iowa_file_path = '../kaggle/input/home-data-for-ml-course/train.csv'
-home_data = pd.read_csv(iowa_file_path)
-X_full = pd.read_csv(iowa_file_path, index_col='Id')
-X_test_full = pd.read_csv(iowa_file_path, index_col='Id')
+train_file_path = '../kaggle/input/home-data-for-ml-course/train.csv'
+home_data = pd.read_csv(train_file_path)
+X_full = pd.read_csv(train_file_path, index_col='Id')
+X_test_full = pd.read_csv(train_file_path, index_col='Id')
 X_full.head()
 X_test_full.head()
+
+# 1. Separate target from predictors
+y = X_full['SalePrice'] #defining target variable
+X_full.drop('SalePrice', axis=1, inplace=True) #dropping target variable from features
+
+# 2. Separete categorical and numerical columns
+categorical_cols = [col for col in X_full.columns if
+                    (X_full[col].dtype == "object")]
+numerical_cols = [col for col in X_full.columns if
+                  X_full[col].dtype in ['int64', 'float64'] and
+                  col != "GarageYrBlt"]
+
